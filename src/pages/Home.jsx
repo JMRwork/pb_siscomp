40e1ddb28aa1c20423154f875b8bd85d5d196d94
José Marco Ribeiro de Menezes <josemarcor12@gmail.com>
2024-08-26 20:31:23 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ export default function Home() {
     return (
         <div>
             <h2>Sistema de Compras</h2>
+            <p>Usuario do admin: 'admin@email.com'</p>
+            <p>Senha do admin: 'password'</p>
         </div>
     );
 }
